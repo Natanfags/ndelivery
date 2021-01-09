da2f5858884ed0065e6c647a9deafe7fad060d4a
Natan Fagundes <natan.fagundes@elotech.com.br>
2021-01-09 15:36:53 -0300

    create project

---
+++ backend/src/main/java/com/natan/ndelivery/NdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.natan.ndelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/natan/ndelivery/NdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natan.ndelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
